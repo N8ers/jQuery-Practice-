43dfc335305cc65a7e94d1524284c17f1ec1ae30
N8ers <nathanraysheryak@gmail.com>
2018-06-28 16:17:52 -0400

    jQuery done for day

--- selectors/index.js
+++ selectors/index.js
@@ -1,3 +1,9 @@
 console.log("Check check - javascript is properly linked :)");
 
-$("h1#heading1").hide();
+$('p span').css('color', 'red');
+$('ul#list li:first').css('color', 'red');
+$('ul#list li:odd').css('color', 'green');
+$('ul#list li:nth-child(3n)').css('list-style', 'none');
+$('[href]').css('color', 'red');
+$('a[href="http://google.com"]').css('color', 'green');
+// $('*').hide();
